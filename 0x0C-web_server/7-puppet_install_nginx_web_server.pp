6a75e9ecbdfe2d59d05484986d8e1dc574c0c5d0
meriembenayad <meriem.benayad@gmail.com>
2023-11-28 21:49:22 +0100

    Task 5: Install nginx web server (w/Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,35 @@
-# Configure server with Puppet
-class { 'nginx' }
-
-nginx::resource::server { 'default':
-    listen_port	=> 80,
-    index_files	=> ['index.html'],
-    www_root	=> '/var/www/html',
+# Install nginx
+package { 'nginx':
+  ensure => 'installed',
 }
 
+# file index
 file { '/var/www/html/index.html':
-    ensure	=> file,
-    content	=> 'Hello World!',
+  ensure  => 'file',
+  content => 'Hello World!',
+  mode    => '0644',
+  require => Package['nginx'],
 }
 
-nginx::resource::location { 'redirect_me':
-    location => '/redirect_me',
-    server => 'default',
-    return => '301 return https://www.youtube.com/watch?v=QH2-TGUlwu4',
-    location_alias => '/var/www/html',
+# redirect_me file
+file_line { 'redirect_me':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;'
+  line   => '
+    location /redirect_me {
+      return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }',
+}
+
+# Stop nginx
+exec { 'stop service':
+  command => 'sudo service nginx stop',
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+}
+
+# Run nginx
+exec { 'start service':
+  command => 'sudo service nginx start',
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
 }
