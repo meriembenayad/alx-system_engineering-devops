3aff4065d0a987c42ae4ab69b29b9a637c8a30e8
meriembenayad <meriem.benayad@gmail.com>
2024-01-17 02:01:17 +0100

    Task 1: How many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,13 @@ import requests
 def number_of_subscribers(subreddit):
     """ Return number of subscriber """
     api_url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {"User-Agent": "Mira"}
+    headers = {
+        "User-Agent": "Mira"
+    }
     response = requests.get(api_url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
     if (response.status_code == 200):
         data = response.json()
         return data['data']['subscribers']
-    else:
-        return 0
+    return 0
