e01e89a5aee4d16e49d81b3b36c85e6d3e6bc5be
meriembenayad <meriem.benayad@gmail.com>
2024-01-17 02:20:32 +0100

    Task 1: Top 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
-"""  """
+""" Query the Reddit API and prints the titles of the first 10 hot posts """
+
+import requests
+
+
+def top_ten(subreddit):
+    """ Return top 10 hot posts titles """
+    api_url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {
+        "User-Agent": "Mira top"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(api_url, headers=headers,
+                            params=params, allow_redirects=False)
+    if response.status_code != 200:
+        print('None')
+
+    data = response.json()['data']['children']
+    for post in data:
+        print(post['data']['title'])
